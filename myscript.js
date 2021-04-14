d211c03e1b53627596bd6e2793b6f4f223ef6c48
Akissan <astellakissan@gmail.com>
2021-04-15 00:43:06 +0300

    lesson2

--- myscript.js
+++ myscript.js
@@ -1,30 +1,57 @@
-let buttonPressCounter = 0; // Исполняется в самом самом начале, когда страница отображается первый раз
-let putin = { deistvie: "\n pomogi", national_democrat: "228" }
 function buttonClicked() {
-  let defaultColor = document.body.style.backgroundColor;
-  document.body.style.backgroundColor = "grey";
-  //let putin = { deistvie: "\n pomogi", national_democrat: "228" }
-  let Nameste = {streamsniper: "\n DeadForsen"}
-
-  alert("Gay website x" + buttonPressCounter+ "\n" +"\nPomosh"+ "\n Yay" + putin.deistvie + " " + putin.national_democrat );
-  
-  document.body.style.backgroundColor = defaultColor;
-
-  buttonPressCounter += 1;
-  console.log(buttonPressCounter); //F12 -> Console
-  //Timer(putin)
-  VI(Nameste)
-
-}
-function Van(Zvezda) {
-
-	alert("0" + " " + "pomoshi" + Zvezda.deistvie);
-    
-}
-function VI(WON) {
-	
-	alert(WON.streamsniper);
+  alert("buttonClicked");
 }
 
-let A = [6,3,1];
-A[0];
+let user_name = "John";
+let user_age = 25;
+
+/*
+let user = { name: "Tommy", age: 25 };
+console.log(user.name);
+console.log(user.age);
+
+/*
+let simple_car = { color: "red", power: 658 };
+
+/*
+let user_2 = { name: "Edos", age: 19, car: simple_car };
+
+/*
+let user_3 = { name: "Edos", age: 19, car: { color: "blue", power: 705 } };
+
+/*
+function foo(a) {
+  console.log(a);
+}
+
+/*
+function bar(a, b) {
+  return a + b;
+}
+
+/*
+function bar_minus(a, b) {
+  return a - b;
+}
+
+/*
+function daz(cool_func, c, d) {
+  return cool_func(c, d);
+}
+
+
+/*
+function daz(cool_func, c, d) {
+  let result = cool_func(c, d);
+  return result;
+}
+*/
+
+//let z = bar(5, 6);
+//foo(z);
+
+// foo(bar(5, 7));
+
+// foo(daz(bar, 3, 2));
+
+// foo(daz(daz, 3, 2));
