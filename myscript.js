0b0fd0a97e5815efaecf051fbb9aeba38f5a2694
otecgeogriy <vitek_7_77@mail.ru>
2021-04-15 00:38:34 +0300

    suicide

--- myscript.js
+++ myscript.js
@@ -1,13 +1,30 @@
 let buttonPressCounter = 0; // Исполняется в самом самом начале, когда страница отображается первый раз
-
+let putin = { deistvie: "\n pomogi", national_democrat: "228" }
 function buttonClicked() {
   let defaultColor = document.body.style.backgroundColor;
-  document.body.style.backgroundColor = "pink";
+  document.body.style.backgroundColor = "grey";
+  //let putin = { deistvie: "\n pomogi", national_democrat: "228" }
+  let Nameste = {streamsniper: "\n DeadForsen"}
 
-  alert("AAAHHHHHHHHHHHH x" + buttonPressCounter);
-
+  alert("Gay website x" + buttonPressCounter+ "\n" +"\nPomosh"+ "\n Yay" + putin.deistvie + " " + putin.national_democrat );
+  
   document.body.style.backgroundColor = defaultColor;
 
   buttonPressCounter += 1;
   console.log(buttonPressCounter); //F12 -> Console
+  //Timer(putin)
+  VI(Nameste)
+
 }
+function Van(Zvezda) {
+
+	alert("0" + " " + "pomoshi" + Zvezda.deistvie);
+    
+}
+function VI(WON) {
+	
+	alert(WON.streamsniper);
+}
+
+let A = [6,3,1];
+A[0];
