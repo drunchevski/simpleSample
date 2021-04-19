293b52603de8d7b2e2bd4fff97bea84aa322e04f
Akissan <astellakissan@gmail.com>
2021-04-19 21:17:41 +0300

    task 6

---
+++ tasks/task_6.js
@@ -0,0 +1,12 @@
+const inpS1 = "is this reversed? tenet";
+let resStr = "";
+
+// FOR IN
+
+console.log(resStr);
+
+/*
+console.log(
+  [...inpS1].map((si, idx, sinp) => sinp[sinp.length - idx - 1]).join("")
+);
+*/
