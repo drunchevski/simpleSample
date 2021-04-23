9db9a7c6ffa508394115565962042ea40305350c
otecgeogriy <vitek_7_77@mail.ru>
2021-04-24 01:07:38 +0300

    aboba

--- myscript.js
+++ myscript.js
@@ -1,18 +1,46 @@
 function removeTask(event) {
   console.log(event);
-  /*
-  let taskList = document.getElementById("taskList");
+  
+  let xXx_Delete_xXx = event.target
+  xXx_Delete_xXx.removeChild(xXx_Delete_xXx.childNodes[0]);
+  console.log(xXx_Delete_xXx);
+
+
+  let taskText = document.createTextNode("НЫААААААААААААААА");
+  event.target.appendChild(taskText);
+
+  
+  /*let taskList = document.getElementById("taskList");
   taskList.removeChild(event.target);
-  */
+ */ 
+}
+function PrisivPapicha() {
+let gitPush = document.getElementById("Pukich");
+let gitgut = document.createElement("img");
+let taskInput = document.getElementById("taskInput");
+let task = taskInput.value; 
+let IWidth = task;
+let Iheight = "84.3833px"; 
+let massiv = ["pap","papik","papzan","papicio"];
+let Rand = Math.floor(Math.random() *4);
+let Randy = massiv[Rand];
+
+gitgut.setAttribute("src", Randy+".gif"); 
+gitgut.setAttribute("width", IWidth); 
+gitgut.setAttribute("height",Iheight)
+gitPush.appendChild(gitgut); 
+//Math.floor(Math.random() * 5); // 0 1 2 3 4
+
 }
 
 function addTask() {
   let taskInput = document.getElementById("taskInput");
-  let task = taskInput.value;
+  let task = taskInput.value;  
 
   let taskList = document.getElementById("taskList");
 
   let taskText = document.createTextNode(task);
+  
   let taskNode = document.createElement("h4");
   taskNode.setAttribute("class", "task panel");
   taskNode.onclick = removeTask;
